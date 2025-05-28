a275b3922787d5a64dcd5b83c4a8e2440c9c05bf
Jaynice <janyce.wathuo@student.moringaschool.com>
2025-05-28 23:39:43 +0300

    Initial commit

--- customer.py
+++ customer.py
@@ -1,44 +1,18 @@
-from order import Order
-
 class Customer:
-    def __init__(self, name):
-        if not isinstance(name, str):
-            raise TypeError("Name must be a string")
-        if not (1 <= len(name) <= 15):
-            raise ValueError("Name must be between 1 and 15 characters")
-        self._name = name
-        self._orders = []
+    def __init__(self,name):
+        self.name = name
 
     @property
     def name(self):
         return self._name
-
+    
     @name.setter
-    def name(self, new_name):
-        if not isinstance(new_name, str):
-            raise TypeError("Name must be a string")
-        if not (1 <= len(new_name) <= 15):
-            raise ValueError("Name must be between 1 and 15 characters")
-        self._name = new_name
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <=15:
+            self._name = name
+        else:
+            raise ValueError("Name must be a string between 1 and 15 characters.")
+        
 
-    def create_order(self, coffee, price):
-        order = Order(self, coffee, price)
-        self._orders.append(order)
-        return order
-
-    def orders(self):
-        return self._orders
-
-    def coffees(self):
-        return list(set(order.coffee for order in self._orders))
-
-    @classmethod
-    def most_aficionado(cls, coffee):
-        highest = None
-        highest_total = 0
-        for customer in set(order.customer for order in coffee.orders()):
-            total = sum(order.price for order in customer.orders() if order.coffee == coffee)
-            if total > highest_total:
-                highest_total = total
-                highest = customer
-        return highest
+customer1 = Customer("Jerome")
+print(customer1.name)
